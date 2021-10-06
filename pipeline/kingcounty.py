e3d6d7397784b24c9be89077318f6683f07a7f21
Egemen Pamukcu <egemenpamukcu@PHS-ML-14.local>
2021-10-06 15:18:01 -0500

    king county scraper and sample data

---
+++ pipeline/kingcounty.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import requests
+from io import StringIO
+from tqdm import tqdm
+import os
+import json
+from datetime import datetime
+import time
+
+
+def get_all_data(start_date):
+    url = 'https://green2.kingcounty.gov/marine-buoy/Data.aspx'
+    
+    # Setting up the parameters for POST request
+    path = os.path.join('..', 'data', 'king-county', 'keys', 'king-county-keys.json')
+    with open(path) as f:
+        params = json.load(f)
+    
+    start_date_key = 'ctl00$kcMasterPagePlaceHolder$startDate'
+    end_date_key = 'ctl00$kcMasterPagePlaceHolder$endDate'
+    current_time = datetime.now()
+    current_date = current_time.strftime('%m/%d/%Y')
+    
+    dfs = []
+    for buoy, data in params.items():
+        data[start_date_key] = start_date
+        data[end_date_key] = current_date
+        response = requests.post(url, data=data)
+        tsv = response.text.split('***END***')[1]
+        df = pd.read_csv(StringIO(tsv), sep='\t')
+        df['STATION_NAME'] = buoy
+        df.dropna(how='all', axis=1, inplace=True)
+        col_renames = {c: 'Measure_' + c for c in df.columns if not 
+                      ((c.startswith('Qual')) | (c == 'Date') | (c == 'STATION_NAME'))}
+        df.rename(columns = col_renames, inplace=True)
+        dfs.append(df)
+    return(pd.concat(dfs))
+
+
+if __name__ == '__main__':
+	df = get_all_data('01/01/2021')
+	path = os.path.join('..', 'data', 'king-county',
+		'king_county_data_{}.csv'.format(str(int(time.time()))))
+	df.to_csv(path, index=False)
