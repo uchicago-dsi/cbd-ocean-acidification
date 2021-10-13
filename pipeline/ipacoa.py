d6725ed14852755c464525ec5107c2ce07c94870
Egemen Pamukcu <egemenpamukcu@PHS-ML-14.local>
2021-10-13 12:56:48 -0500

    changed docstrings, fixed paths

--- pipeline/ipacoa.py
+++ pipeline/ipacoa.py
@@ -5,55 +5,69 @@ from tqdm import tqdm
 import time
 import os
 
-PLATFORM_URL = 'http://www.ipacoa.org/ssa/get_platform_data.php'
-PLATFORMS_PATH = os.path.join('..', 'data', 'ipacoa', 'lookups', 'platform_measurements.csv')
+PATH = os.path.abspath(__file__)
 
-def get_historical_data(path=PLATFORMS_PATH, url=PLATFORM_URL):
-	'''
-	Queries IPACOA to get information on various environmental measurements. 
+def get_data(station_id=None, start_date=None, end_date=None):
+    """ Retrieves data for input station(s) and time range as DataFrame.
 
-	Inputs:
-		path (str): Relative path directing to the location of a table containing
-			all platform-measurement combinations to query. 
-		url (str): IPACOA URL for requesting historical data from platforms. 
-	Output:
-		Pandas DataFrame: Contains information on all platforms listed in the input csv.
-	'''
+    Args:
+        station_id (str): Default None. If none supplied, retrieves data
+            for all station_ids
+        start_date (MM/DD/YYYY): Default None. If none supplied, 
+            starts with earliest available
+        end_date(MM/DD/YYYY): Default None. If none, uses current date.
+    Returns:
+        pd.DataFrame: Contains information on all platforms listed in the input csv.
+    """
 
-	platform_measurement = pd.read_csv(PLATFORMS_PATH)
-	dfs = []
-	for i, (platform, measurement) in tqdm(platform_measurement.iterrows(), 
-										   total=platform_measurement.shape[0]):
-		params = (
-			('platform_id', platform),
-			('var_id', measurement),
-			('data_type', 'csv'),
-		)
-		response = requests.get(PLATFORM_URL, params=params)
-		
-		# Raise error if request is not successful
-		response.raise_for_status()
+    url = 'http://www.ipacoa.org/ssa/get_platform_data.php'
+    measurements_path = os.path.join(PATH, '..', '..', 'data', 'ipacoa', 'lookups', 
+        'platform_measurements.csv')
 
-		if response.text == '': 
-			continue
-		df = pd.read_csv(StringIO(response.text))
-		df['platform_id'] = platform
-		df['value'] = df.pop(df.columns[2])
-		df['measurement_id'] = measurement
-		df.rename(columns={' Depth (Ft)': 'depth_ft',
-						  'Date and Time': 'date_time'},
-				 inplace=True)
-		df['depth_ft'] = df['depth_ft'].str.strip(' ft').astype(int)
-		df = df[['date_time', 'platform_id', 'measurement_id', 'depth_ft', 'value']]
-		dfs.append(df)
+    # Setting up parameters for GET request
+    platform_measurement = pd.read_csv(path)
+    if station_id:
+        mask = platform_measurement['platform_label'] == station_id
+        platform_measurement = platform_measurement[mask]
+    
+    # Iterate over platform * measurement combinations
+    dfs = []
+    for i, (platform, measurement) in tqdm(platform_measurement.iterrows(), 
+                                           total=platform_measurement.shape[0]):
+        params = (
+            ('platform_id', platform),
+            ('var_id', measurement),
+            ('data_type', 'csv'),
+        )
+        response = requests.get(url, params=params)
+        
+        # Raise error if request is not successful
+        response.raise_for_status()
 
-	all_measures = pd.concat(dfs, ignore_index=True)
-	return all_measures
+        if response.text == '': 
+            continue
+        df = pd.read_csv(StringIO(response.text))
+        df['platform_id'] = platform
+        df['value'] = df.pop(df.columns[2])
+        df['measurement_id'] = measurement
+        df.rename(columns={' Depth (Ft)': 'depth_ft',
+                          'Date and Time': 'date_time'},
+                 inplace=True)
+        df['depth_ft'] = df['depth_ft'].str.strip(' ft').astype(int)
+        df = df[['date_time', 'platform_id', 'measurement_id', 'depth_ft', 'value']]
+        dfs.append(df)
+
+    all_measures = pd.concat(dfs, ignore_index=True)
+    if start_date:
+        start_date = pd.to_datetime(start_date)
+        all_measures = all_measures[all_measures['date_time'] >= start_date]
+    if end_date:
+        end_date = pd.to_datetime(end_date)
+        all_measures = all_measures[all_measures['date_time'] <= end_date]
+    return all_measures
 
 if __name__ == "__main__":
-	df_all = get_historical_data()
-	path = os.path.join('..', 'data', 'ipacoa',
-		'ipacoa_data_{}.csv'.format(str(int(time.time()))))
-	df_all.to_csv(path, index=False)
-
-
+    df_all = get_data()
+    data_path = os.path.join(PATH, '..', '..', 'data', 'ipacoa',
+        'ipacoa_data_{}.csv'.format(str(int(time.time()))))
+    df_all.to_csv(path, index=False)
--- pipeline/kingcounty.py
+++ pipeline/kingcounty.py
@@ -6,38 +6,45 @@ import os
 import json
 from datetime import datetime
 import time
+import re
 
+PATH = os.path.abspath(__file__)
 
-def get_all_data(start_date):
-    '''
-    Queries King County to get environmental measurements from Dockton, Point Williams, 
-    Seattle Aquarium, and Quartermaster Yacht Club through a POST request. 
+def get_data(station_id=None, start_date=None, end_date=None):
+    """ Retrieves data for input station(s) and time range as DataFrame.
 
-    Input:
-        start_date (str): The start date for data in 'MM/DD/YYYY' format. 
-    Output:
-        Pandas DataFrame: Contains information on all platforms listed in the input csv.
-    '''
-    if not re.match(r'\d{2}/\d{2}/\d{4}', start_date):
-        raise TypeError('Wrong format for start date, format must be MM/DD/YYYY.')
-
+    Args:
+        station_id (str): Default None. If none supplied, retrieves data
+            for all station_ids
+        start_date (MM/DD/YYYY): Default None. If none supplied, 
+            starts with earliest available
+        end_date(MM/DD/YYYY): Default None. If none, uses current date.
+    Returns:
+        pd.DataFrame: Contains information on all platforms listed in the input json.
+    """
 
     url = 'https://green2.kingcounty.gov/marine-buoy/Data.aspx'
-    
+    param_path = os.path.join(PATH, '..', '..', 'data', 'king-county', 'keys', 
+        'king-county-keys.json')
+
+    if not end_date:
+        current_time = datetime.now()
+        end_date = current_time.strftime('%m/%d/%Y')
+    if not start_date:
+        start_date = '01/01/1900'
+
     # Setting up the parameters for POST request
-    path = os.path.join('..', 'data', 'king-county', 'keys', 'king-county-keys.json')
-    with open(path) as f:
+    with open(pram_path) as f:
         params = json.load(f)
     
     start_date_key = 'ctl00$kcMasterPagePlaceHolder$startDate'
     end_date_key = 'ctl00$kcMasterPagePlaceHolder$endDate'
-    current_time = datetime.now()
-    current_date = current_time.strftime('%m/%d/%Y')
-    
+
+    # Iterate over station parameters
     dfs = []
     for buoy, data in tqdm(params.items()):
         data[start_date_key] = start_date
-        data[end_date_key] = current_date
+        data[end_date_key] = end_date
         response = requests.post(url, data=data)
         tsv = response.text.split('***END***')[1]
         df = pd.read_csv(StringIO(tsv), sep='\t', low_memory=False)
@@ -47,11 +54,13 @@ def get_all_data(start_date):
                       ((c.startswith('Qual')) | (c == 'Date') | (c == 'STATION_NAME'))}
         df.rename(columns = col_renames, inplace=True)
         dfs.append(df)
-    return(pd.concat(dfs))
+
+    all_measures = pd.concat(dfs, ignore_index=True)
+    return(all_measures)
 
 
 if __name__ == '__main__':
-	df = get_all_data(input('Enter beginning date in format MM/DD/YYYY:'))
-	path = os.path.join('..', 'data', 'king-county',
-		'king_county_data_{}.csv'.format(str(int(time.time()))))
-	df.to_csv(path, index=False)
+    df = get_data()
+    data_path = os.path.join(PATH, '..', '..', 'data', 'king-county',
+        'king_county_data_{}.csv'.format(str(int(time.time()))))
+    df.to_csv(path, index=False)
