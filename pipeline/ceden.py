75650a9fc114bf539b914ee8c8c2585f16b9e9bd
Trevor Spreadbury <trevorspreadbury@gmail.com>
2022-03-01 18:51:32 -0600

    reformat ceden to submit to csvs instead of xls

--- pipeline/ceden.py
+++ pipeline/ceden.py
@@ -1,208 +1,210 @@
 # -*- coding: utf-8 -*-
-
-import utils
+from pipeline import utils
 import pandas as pd
-from xlrd import open_workbook
-from xlutils.copy import copy
-import os
+from datetime import datetime
 import numpy as np
+from pathlib import Path
 from pytemp import pytemp  # for temperature unit conversion
 import time
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
+HERE = Path(__file__).resolve().parent
+stations = HERE / "metadata" / "stations.csv"
+station_parameters = HERE / "metadata" / "station_parameter_metadata.csv"
 
-def populate_locations(df, locations):
-    """ Processes California data to match CEDEN template restrictions.
-        Writes data to the template's 'Locations' sheet. Changes the existing
-        sheet and does not return anything. 
+# .xls does not allow more than 63356 rows
+MAX_EXCEL_SIZE = 65535
 
-    Args:
-        df (DataFrame): Contains the merged columns of 'measurements' 
-            and 'stations' tables for California stations. 
-        locations (xlwt.Worksheet): Excel Worksheet object for the 'Locations' 
-            sheet of the template. 
+location_columns = {
+    "latitude": "ActualLatitude",
+    "longitude": "ActualLongitude",
+    "horizontal_datum": "Datum",
+    "ceden_id": "StationCode",
+    "horizontal_coordinate_collection": "CoordinateSource",
+    "ceden_project_code": "ProjectCode",
+    "source": "AgencyCode"
+}
+
+results_columns = {
+    "instrument": "CollectionDeviceName",
+    "station_id": "StationCode",
+    "parameter": "AnalyteName",
+    "value": "Result",
+    "unit": "UnitName",
+    "quality": "QACode",
+    "depth": "CollectionDepth",
+    "depth_unit": "UnitCollectionDepth",
+    "method": "MethodName",
+}
+
+parameter_dict = {
+    "salinity": "Salinity",
+    "total_alkalinity": "Alkalinity as CaCO3",
+    "water_temperature": "Temperature",
+    "oxygen_concentration": "Oxygen, Dissolved",
+    "oxygen_saturation": "Oxygen, Saturation",
+    "co2": "Carbon dioxide, free",
+    "water_pressure": "Pressure",
+    "air_pressure": "Barometric Pressure",
+    "air_temperature": "Temperature",  # looks like same as water
+    "tco2": "Carbon dioxide, free",
+    "pH": "pH",
+}
+class CEDEN():
+    state = "California"
+
+    def __init__(self):
+        """ initialize with proper output directory """
+        request_time = datetime.now().strftime("%Y-%m-%dT%H-%M")
+        self.results_directory = HERE.parent / "output" / self.state / request_time
+        self.results_directory.mkdir(exist_ok=True, parents=True)
+
+    def format_data_for_agency(self, data: pd.DataFrame) -> Path:
+        """ Outermost method for transforming data into agency ready format
         
-    Returns:
-        None
-    """
+        Args:
+            data: data from collector subclass in standardized format 
+        Returns:
+            nothing. Creates directory with results.
+        """
+        split_n = data.shape[0] // MAX_EXCEL_SIZE + 1
+        dfs = np.array_split(data, split_n)
 
-    # Preprocess the dataframe to match CEDEN format
-    df = df[
-        ["station_id", "latitude", "longitude", "date", "provider"]
-    ].drop_duplicates()
-    df["StationCode"] = df.pop("station_id").map(utils.ceden_stations)
-    df["Datum"] = df["provider"].map(utils.ceden_datum_dict)
+        for batch_no, df in enumerate(dfs):
+            stations_table = pd.read_csv(stations, index_col="station_id")
+            stations_used = df["station_id"].unique()
+            stations_subset = stations_table[stations_table.index.isin(stations_used)]
+            locations = self.populate_locations(stations_subset)
+            results = self.populate_field_results(df)
+            location_file = self.results_directory / "cbd_locations_b{}.csv".format(batch_no)
+            locations.to_csv(location_file)
+            results_file = self.results_directory / "cbd_results_b{}.csv".format(batch_no)
+            results.to_csv(results_file)
+        return self.results_directory
 
-    for k, v in utils.ceden_station_misc.items():
-        df.loc[:, k] = v
+    def populate_locations(self, df: pd.DataFrame):
+        """ Processes California data to match CEDEN template restrictions.
 
-    df = df[
-        [
-            "StationCode",
-            "date",
-            "ProjectCode",
-            "EventCode",
-            "ProtocolCode",
-            "AgencyCode",
-            "SampleComments",
-            "LocationCode",
-            "GeometryShape",
-            "CoordinateNumber",
-            "latitude",
-            "longitude",
-            "Datum",
-            "CoordinateSource",
-            "Elevation",
-            "UnitElevation",
-            "StationDetailVerBy",
-            "StationDetailVerDate",
-            "StationDetailComments",
+        Args:
+            df (DataFrame): Contains the merged columns of 'measurements' 
+                and 'stations' tables for California stations. 
+            
+        Returns:
+            locations_dataframe
+        """
+        df = df.reset_index()
+        df["ceden_id"] = np.where(df["ceden_id"], df["ceden_id"], df["station_id"])
+        # Preprocess the dataframe to match CEDEN format
+        df = df[
+            ["ceden_id", "latitude", "longitude", "source", "horizontal_datum", "ceden_project_code", "horizontal_coordinate_collection"]
         ]
-    ]
+        df.rename(columns=location_columns, inplace=True, errors="ignore")
+        df["CoordinateNumber"] = 1
+        df["LocationCode"] = "Not Recorded"
+        df["EventCode"] = "WQ"
+        df["ProtocolCode"] = "Not Recorded"
+        df["GeometryShape"] = "Point"
 
-    # Write dataframe content to the 'Locations' sheet of CEDEN template
-    iter_stations = df.reset_index(drop=True).iterrows()
-    for i, cols in iter_stations:
-        for j, value in enumerate(cols):
-            locations.write(i + 1, j, value)
+        df = df.loc[:, df.columns.isin(
+            [
+                "StationCode",
+                "SampleDate",
+                "ProjectCode",
+                "EventCode",
+                "ProtocolCode",
+                "AgencyCode",
+                "SampleComments",
+                "LocationCode",
+                "GeometryShape",
+                "CoordinateNumber",
+                "ActualLatitude",
+                "ActualLongitude",
+                "Datum",
+                "CoordinateSource",
+                "Elevation",
+                "UnitElevation",
+                "StationDetailVerBy",
+                "StationDetailVerDate",
+                "StationDetailComments",
+            ]
+        )]
+
+        return df
 
 
-def populate_field_results(df, field_results):
-    """ Processes California data to match CEDEN template restrictions.
-        Writes data to the template's 'Field Results' sheet. Changes the existing
-        sheet and does not return anything. 
+    def populate_field_results(self, df):
+        """ Processes California data to match CEDEN template restrictions.
 
-    Args:
-        df (DataFrame): Contains the merged columns of 'measurements' 
-            and 'stations' tables for California stations. 
-        field_results (xlwt.Worksheet): Excel Worksheet object for the 'Field results' 
-            sheet of the template. 
+        Args:
+            df (DataFrame): Contains the merged columns of 'measurements' 
+                and 'stations' tables for California stations.             
+        Returns:
+            field_results dataframe
+        """
+        stations_table = pd.read_csv(stations)
+        stations_table["ceden_id"] = np.where(
+            stations_table["ceden_id"],
+            stations_table["ceden_id"], 
+            stations_table["station_id"]
+        )
+        df["station_id"] = df["station_id"].map(stations_table.set_index("station_id")["ceden_id"])
+        df["CollectionTime"] = pd.to_datetime(df["datetime"], utc=True).dt.strftime("%H:%M")
+        df["SampleDate"] = pd.to_datetime(df["datetime"], utc=True).dt.strftime("%d/%m/%Y")
+        df["MatrixName"] = df["parameter"].map(utils.ceden_matrix_dict)
+        df["Replicate"] = df.groupby(["station_id", "SampleDate"]).cumcount() + 1
+        df["instrument"] = np.where(df["instrument"], df["instrument"], "Not Recorded")
+        df["method"] = np.where(df["method"], df["method"], "FieldMeasure")
+
+        molg = df.loc[df.unit == "µmol/kg", "value"] * 1000
+        df.loc[df.unit == "µmol/kg", "value"] = molg
+
+        deg_c = df.loc[df.unit == "F", "value"].apply(lambda x: pytemp(x, "f", "c"))
+        df.loc[df.unit == "F", "value"] = deg_c
+
+        mmhg = df.loc[df.unit == "inHg", "value"] / 0.0393701
+        df.loc[df.unit == "inHg", "value"] = mmhg
+
+        for k, v in utils.ceden_field_misc.items():
+            df.loc[:, k] = v
+
         
-    Returns:
-        None
-    """
-
-    df["time"] = pd.to_datetime(df.date_time, utc=True).dt.strftime("%H:%M")
-    df["Replicate"] = df.groupby(["station_id", "date"]).cumcount() + 1
-
-    df["station_id"] = df["station_id"].map(utils.ceden_stations)
-
-    molg = df.loc[df.unit == "µmol/kg", "value"] * 1000
-    df.loc[df.unit == "µmol/kg", "value"] = molg
-
-    deg_c = df.loc[df.unit == "°F", "value"].apply(lambda x: pytemp(x, "f", "c"))
-    df.loc[df.unit == "°F", "value"] = deg_c
-
-    mmhg = df.loc[df.unit == "inHg", "value"] / 0.0393701
-    df.loc[df.unit == "inHg", "value"] = mmhg
-
-    for k, v in utils.ceden_field_misc.items():
-        df.loc[:, k] = v
-
-    df["MatrixName"] = df["parameter"].map(utils.ceden_matrix_dict)
-    df["unit"] = df["unit"].map(utils.ceden_unit_dict)
-    df["parameter"] = df["parameter"].map(utils.ceden_param_dict)
-    df.sort_values(["station_id", "parameter", "date_time"])
-    df = df[
-        [
-            "station_id",
-            "date",
-            "ProjectCode",
-            "EventCode",
-            "ProtocolCode",
-            "AgencyCode",
-            "SampleComments",
-            "LocationCode",
-            "GeometryShape",
-            "time",
-            "CollectionMethodCode",
-            "Replicate",
-            "CollectionDeviceName",
-            "depth",
-            "depth_unit",
-            "PositionWaterColumn",
-            "FieldCollectionComments",
-            "MatrixName",
-            "MethodName",
-            "parameter",
-            "FractionName",
-            "unit",
-            "FieldReplicate",
-            "value",
-            "ResQualCode",
-            "QACode",
-            "ComplianceCode",
-            "BatchVerificationCode",
-            "CalibrationDate",
-            "FieldResultComments",
-        ]
-    ]
-    iter_measurements = df.reset_index(drop=True).iterrows()
-    for i, cols in iter_measurements:
-        for j, value in enumerate(cols):
-            field_results.write(i + 1, j, value)
-
-
-def populate_template(measurements_path, stations_path, template_path, submittal_path):
-    """ Wrapper function to populate CEDEN template. Reads 'measurements' and 'stations' 
-        tables as well as the empty CEDEN template and passes them as parameters to the
-        populate_locations() and populate_field_results() functions.
-
-    Args:
-        measurements_path (str): Path to the measurements .csv file. 
-        stations_path (str): Path to the stations .csv file. 
-        template_path (str): Path to the empty CEDEN template (.xls).
-        submittal_path (str): Path to the folder where the populated template is going to
-            be saved.  
-        
-    Returns:
-        None. Saves the populated template to the provided template_path.
-    """
-    measurements = pd.read_csv(measurements_path, parse_dates=["date_time"])
-    stations = pd.read_csv(stations_path)
-
-    cali_stations = stations.query('state == "California"')
-    cali_stations = cali_stations.merge(measurements, on="station_id", how="inner")
-    cali_stations.sort_values(["station_id", "parameter", "date_time"], inplace=True)
-    cali_stations["date"] = pd.to_datetime(
-        cali_stations.date_time, utc=True
-    ).dt.strftime("%d/%b/%Y")
-
-    # .xls does not allow more than 63356 rows
-    max_rows = 65535
-    split_n = cali_stations.shape[0] // max_rows + 1
-    dfs = np.array_split(cali_stations, split_n)
-
-    for df in dfs:
-        rb = open_workbook(template_path, formatting_info=True)
-        wb = copy(rb)
-        locations = wb.get_sheet(1)
-        field_results = wb.get_sheet(2)
-
-        populate_locations(df, locations)
-        populate_field_results(df, field_results)
-
-        file_name = "ceden_{}.xls".format(str(int(time.time())))
-        file_path = os.path.join(submittal_path, file_name)
-
-        wb.save(file_path)
-
-
-if __name__ == "__main__":
-    PATH = os.path.abspath(__file__)
-
-    measurements_path = os.path.abspath(
-        os.path.join(PATH, "..", "..", "data", "measurements.csv")
-    )
-    stations_path = os.path.abspath(
-        os.path.join(PATH, "..", "..", "data", "stations.csv")
-    )
-    template_path = os.path.abspath(
-        os.path.join(PATH, "..", "..", "templates", "ceden_field_template.xls")
-    )
-    submittal_path = os.path.abspath(
-        os.path.join(PATH, "..", "..", "data", "submittal")
-    )
-
-    populate_template(measurements_path, stations_path, template_path, submittal_path)
+        df["unit"] = df["unit"].map(utils.ceden_unit_dict)
+        df["parameter"] = df["parameter"].map(parameter_dict)
+        df.sort_values(["station_id", "parameter", "datetime"])
+        df.rename(columns=results_columns, inplace=True, errors="ignore")
+        df = df.loc[:, df.columns.isin(
+            [
+                "StationCode",
+                "SampleDate",
+                "ProjectCode",
+                "EventCode",
+                "ProtocolCode",
+                "AgencyCode",
+                "SampleComments",
+                "LocationCode",
+                "GeometryShape",
+                "CollectionTime",
+                "CollectionMethodCode",
+                "Replicate",
+                "CollectionDeviceName",
+                "CollectionDepth",
+                "UnitCollectionDepth",
+                "PositionWaterColumn",
+                "FieldCollectionComments",
+                "MatrixName",
+                "MethodName",
+                "AnalyteName",
+                "FractionName",
+                "UnitName",
+                "FieldReplicate",
+                "Result",
+                "ResQualCode",
+                "QACode",
+                "ComplianceCode",
+                "BatchVerificationCode",
+                "CalibrationDate",
+                "FieldResultComments",
+            ]
+        )]
+        return df
