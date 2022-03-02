f66cb9043afd27f5b390bfa633aea89a51c84273
Trevor Spreadbury <trevorspreadbury@gmail.com>
2022-03-01 18:50:33 -0600

    create logic for running of full pipeline

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,9 @@
 # syntax=docker/dockerfile:1
-
-FROM python:3.8.12-slim-buster
+FROM python:3.9-slim-bullseye
 WORKDIR /src
 RUN pip install --upgrade pip
 COPY requirements.txt requirements.txt
-RUN pip install -r requirements.txt
+RUN python -m pip install --no-cache-dir -r requirements.txt
 COPY . .
-COPY ./shell.sh /
-RUN chmod +x /shell.sh
-CMD [ "/shell.sh" ]
+RUN python setup.py install
+ENTRYPOINT [ "python", "./main.py" ]
---
+++ main.py
@@ -0,0 +1,89 @@
+import argparse
+from datetime import datetime, timedelta
+from distutils.log import error
+from os import stat
+from requests.exceptions import HTTPError
+import pandas as pd
+from pathlib import Path
+
+from pipeline.erddap import ERDDAP
+from pipeline.ipacoa import IPACOA
+from pipeline.kingcounty import KingCounty
+from pipeline.eim import EIM
+from pipeline.ceden import CEDEN
+
+HERE = Path(__file__).resolve().parent
+STATIONS = HERE / 'pipeline' / 'metadata' / 'stations.csv'
+
+collectors = {
+    "IPACOA": IPACOA(),
+    "OOI": ERDDAP("https://erddap.dataexplorer.oceanobservatories.org/erddap/"),
+    "CeNCOOS": ERDDAP("https://erddap.cencoos.org/erddap/"),
+    "King County": KingCounty()
+}
+
+formatters = {
+    "Washington": EIM()
+}
+
+def collect_data(state, start_time, end_time):
+    """ Collects all data from state in time period 
+    
+    Args:
+        state (str): One of 'California', 'Washington', or 'Hawaii'
+            All stations in stations.csv from this state will be queried
+            for the input time period. 
+        start_time (datetime):  earliest date from which to collect
+        end_time (datetime):  latest date from which to collect 
+    Returns:
+        data (pd.DataFrame): Table containing all data points
+    """
+    print(state)
+    stations = pd.read_csv(STATIONS, index_col="station_id")
+    state_stations = stations#[stations['state'] == state]
+    all_station_data = []
+    for index, row in state_stations.iterrows():
+        collector = collectors[row["provider"]]
+        try:
+            station_data = collector.get_data(index, start_time, end_time)
+        except HTTPError as e:
+            continue
+        all_station_data.append(station_data)
+    data = pd.concat(all_station_data)
+    return data
+
+def format_data(state, data):
+    """ Formats input data according to state's specifications
+    
+    Args:
+        state (str): One of 'California', 'Washington', or 'Hawaii'
+        data (pd.DataFrame): Table containing relevant observations
+    Returns:
+        Nothing. Saves relevant documents to folder with name {state}-{unixtime}
+    """
+    formatter = formatters[state]
+    formatter.format_data_for_agency(data)
+    
+
+if __name__ == "__main__":
+    # set up for command line arguments
+    parser = argparse.ArgumentParser(
+        description="Automated oceanographic data collection for 303(d) reviews"
+    )
+    parser.add_argument("state", metavar="STATE", type=str,
+        help="State from which to gather and prepare data"
+    )
+    parser.add_argument("--start", type=str, default=None,
+        help="YYYY/MM/DD. Earliest time from which to gather data. Default 30 days ago."
+    )
+    parser.add_argument("--end", type=str,
+        help="YYYY/MM/DD. Latest time from which to gather data. Default today.")
+    args = parser.parse_args()
+    # set defaults
+    if args.start == None:
+        args.start = datetime.now() - timedelta(30)
+    if args.end == None:
+        args.end = datetime.now()
+    # run pipeline
+    data = collect_data(args.state, args.start, args.end)
+    format_data(args.state, data)
